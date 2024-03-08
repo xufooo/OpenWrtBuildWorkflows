90cfa494f1d40cfe0d657a4dfc8482842e4e1793
OoO <xufooo@gmail.com>
2024-03-08 16:39:45 +0800

    Update zybo-immortalwrt-script-2.sh

--- zybo-immortalwrt-script-2.sh
+++ zybo-immortalwrt-script-2.sh
@@ -24,4 +24,4 @@ sed -i 's/\/var\/adguardhome/\/etc\/AdGuardHome/g' feeds/packages/net/adguardhom
 sed -i 's/adguardhome.yaml/AdGuardHome.yaml/g' feeds/packages/net/adguardhome/files/adguardhome.init
 
 # Fix mosdns conflict with luci-app-mosdns
-cp feeds/small/luci-app-mosdns/Makefile feeds/packages/net/mosdns/Makefile 
+cp feeds/small/mosdns/Makefile feeds/packages/net/mosdns/Makefile 
