c52c0a416d185dd9654fa4b914f60c39a7ddfeab
OoO <xufooo@gmail.com>
2024-04-03 14:23:27 +0800

    Update newifimini-lede-script-1.sh

--- newifimini-lede-script-1.sh
+++ newifimini-lede-script-1.sh
@@ -38,4 +38,5 @@
 #git apply example.patch
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
