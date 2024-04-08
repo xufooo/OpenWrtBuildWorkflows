e0f86fcb605b41bca588f2888524e7524c498f08
OoO <xufooo@gmail.com>
2024-04-08 15:15:04 +0800

    Update wnr2000v3-openwrt-script-2.sh

--- wnr2000v3-openwrt-script-2.sh
+++ wnr2000v3-openwrt-script-2.sh
@@ -23,5 +23,5 @@ mkdir package/network/config/firewall/patches
 wget -P package/network/config/firewall/patches/ https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/fullconenat.patch
 # Patch LuCI
 pushd feeds/luci
-wget -O- https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/luci.patch | git apply
+wget -O- https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/luci.patch | patch -p1
 popd
