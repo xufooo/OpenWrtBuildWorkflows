7b16589c806e3e01edc78aa6ffc9b3e619b90be8
OoO <xufooo@gmail.com>
2025-03-11 22:25:51 +0800

    Update youkul1-immortalwrt-script-1.sh

--- youkul1-immortalwrt-script-1.sh
+++ youkul1-immortalwrt-script-1.sh
@@ -40,12 +40,3 @@
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# fix v24.10.0 has no shadowsocks-libev
-git clone --filter=blob:none --no-checkout https://github.com/immortalwrt/packages.git
-cd packages 
-git sparse-checkout init --cone
-git sparse-checkout set net/shadowsocks-libev
-git checkout openwrt-23.05 
-cd ..
-mkdir -p feeds/packages/net
-cp -r packages/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
