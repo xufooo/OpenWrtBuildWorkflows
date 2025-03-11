0066e255136416de5dd4d2092c67ee1199a609da
OoO <xufooo@gmail.com>
2025-03-11 21:16:21 +0800

    Update youkul1-immortalwrt-script-2.sh

--- youkul1-immortalwrt-script-2.sh
+++ youkul1-immortalwrt-script-2.sh
@@ -1,21 +1,29 @@
 
-#!/bin/bash
+#!/bin/bash 
 # Copyright (c) 2022-2023 Curious <https://www.curious.host>
+
 #
 # This is free software, licensed under the MIT License.
+
 # See /LICENSE for more information.
+
 # 
-# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# https://github.com/Curious-r/OpenWrtBuildWorkflows 
 # Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+
 #-------------------------------------------------------------------------------------------------------
 #
 #
-# Patching is generally recommended.
+# Patching is generally recommended. 
 # # Here's a template for patching:
-#touch example.patch
-#cat>example.patch<<EOF
-#patch content
+
+#touch example.patch  
+#cat>example.patch<<EOF 
+#patch content 
 #EOF
-#git apply example.patch
+#git apply example.patch 
 # fix v2dat build error
 #sed -i '/PKG_BUILD_DIR/a\PKG_USE_MIPS16:=0' feeds/packages/utils/v2dat/Makefile
+# fix v24.10.0 has no shadowsocks-libev
+cd feeds/packages/net
+svn export https://github.com/immortalwrt/packages/branches/fd36d69974ac2ec1460209f18f126f1bacfc5afb/net/shadowsocks-libev
