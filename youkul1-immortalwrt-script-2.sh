851e871fecb7f6df32bb8879a0bdada4b28d5635
Curious <curious@curious.host>
2026-05-28 09:31:46 +0800

    remove script-2 clone: shadowsocks-libev is already provided by kenzok8/small-package feed (active today)

--- youkul1-immortalwrt-script-2.sh
+++ youkul1-immortalwrt-script-2.sh
@@ -1,39 +1,28 @@
-
-#!/bin/bash  
+#!/bin/bash
 # Copyright (c) 2022-2023 Curious <https://www.curious.host>
-
-
 #
 # This is free software, licensed under the MIT License.
-
-
 # See /LICENSE for more information.
-
-
 # 
-# https://github.com/Curious-r/OpenWrtBuildWorkflows  
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
 # Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
-
-
 #-------------------------------------------------------------------------------------------------------
 #
 #
-# Patching is generally recommended.  
+# Patching is generally recommended.
 # # Here's a template for patching:
 
 
-#touch example.patch      
-#cat>example.patch<<EOF  
-#patch content 
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
 #EOF
-#git apply example.patch 
+#git apply example.patch
 # fix v2dat build error
 #sed -i '/PKG_BUILD_DIR/a\PKG_USE_MIPS16:=0' feeds/packages/utils/v2dat/Makefile
 # fix v24.10.0 has no shadowsocks-libev
 #git clone -b openwrt-23.05 https://github.com/immortalwrt/packages.git pack
 #cp -r pack/net/shadowsocks-libev package/feeds/packages/shadowsocks-libev
 #wget -P package/feeds/packages/shadowsocks-libev/patch/ https://raw.githubusercontent.com/zxlhhyccc/packages-2/3f33259b94c0faf32a363817476ef62d00e71910/net/shadowsocks-libev/patches/101-fix-mbedtls3.6-build.patch
-# shadowsocks-libev from ImmortalWrt official packages at matching version
-git clone --depth 1 --branch v25.12.0 https://github.com/immortalwrt/packages.git /tmp/imm-packages
-cp -r /tmp/imm-packages/net/shadowsocks-libev package/feeds/packages/shadowsocks-libev
-rm -rf /tmp/imm-packages
+# shadowsocks-libev is now provided by kenzok8/small-package feed (added via script-1)
+# No additional clone needed.
